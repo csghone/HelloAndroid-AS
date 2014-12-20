158d05d43416e2b0d22b06a3c212a3cd817b162f
Chaitanya Ghone <csghone@ti.com>
2014-12-20 15:44:18 +0530

    Added Nav Drawer
    Signed-off-by: Chaitanya Ghone <csghone@ti.com>

--- app/src/main/java/csg/HelloAndroid/MainActivity.java
+++ app/src/main/java/csg/HelloAndroid/MainActivity.java
@@ -1,55 +1,81 @@
 package csg.HelloAndroid;
 
 import android.app.Activity;
+import android.app.ActionBar;
+
+import android.content.res.Configuration;
 import android.os.Bundle;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MenuInflater;
-
-import android.widget.Toast;
-import android.widget.CheckBox;
-
-import android.app.ActionBar;
+import android.view.*;
+import android.widget.*;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-
 
 public class MainActivity extends Activity
 {
+    private ActionBarDrawerToggle myDrawerToggle;
+    private DrawerLayout myDrawerLayout;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        ActionBar actionBar = getActionBar();
-        actionBar.setTitle("HELLO");
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_activity_actions, menu);
-        return super.onCreateOptionsMenu(menu);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        myDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        myDrawerToggle = new ActionBarDrawerToggle(
+                    this,                              /* host Activity */
+                    myDrawerLayout,                    /* DrawerLayout object */
+                    R.drawable.ic_drawer,              /* nav drawer image to replace 'Up' caret */
+                    R.string.tempStr1,                 /* "open drawer" description for accessibility */
+                    R.string.tempStr2                  /* "close drawer" description for accessibility */
+                    ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(R.string.tempStr1);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+             public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(R.string.tempStr2);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        myDrawerLayout.setDrawerListener(myDrawerToggle);
+        /**
+         * When using the ActionBarDrawerToggle, you must call it during
+         * onPostCreate() and onConfigurationChanged()...
+         */
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Toast toast;
-
-        // Check which checkbox was clicked
-        switch (item.getItemId()) {
-            case R.id.ActionItem1:
-            case R.id.ActionItem2:
-                    toast = Toast.makeText(this, "Check", Toast.LENGTH_SHORT);
-                    toast.show();
-                break;
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (myDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
-        return true;
-    }    
-    
-    
+        // Handle action buttons
+        switch (item.getItemId()) {
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        myDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        myDrawerToggle.onConfigurationChanged(newConfig);
+    }
 }
 
